d9cb4155826c38d7582dca79706d3aa9ff3cceef
FilipHarald <filip.harald@gmail.com>
2022-05-24 22:06:27 +0200

    halfway to solving day4(?)

--- src/solvers/day4/index.ts
+++ src/solvers/day4/index.ts
@@ -1,10 +1,18 @@
+import parse from './parser';
 
 const NUMBER = '4';
 
-const solve = input => {
-  // console.log(input);
-  const lines = input.split('\n');
-  console.log(lines.length);
+const solve = (input: string) => {
+  const {drawingNumbers, boards} = parse(input);
+  const {winnerIndex, drawnNumbers} = drawingNumbers.reduce((acc, nbr) => {
+    // TODO: if winner: just return
+    const mostMarksInRows = boards.map(board => {
+      // TODO: find and mark
+      // TODO: return amount of marks in "best" row
+    });
+    // TODO: if marks === 5 set winnerIndex
+    return acc;
+  }, {drawnNumbers: []});
 };
 
 export default {
---
+++ src/solvers/day4/parser.ts
@@ -0,0 +1,32 @@
+const getNumbersFromLine = (row: string) => {
+  return row.split(' ').reduce((acc, part) => {
+    if (part !== '') {
+      acc.push(part);
+    }
+    return acc;
+  }, [])
+};
+
+const parse = (input: string) => {
+  // console.log(input);
+  const lines = input.split('\n');
+  // console.log(lines);
+  const drawingNumbers = lines.shift().split(',');
+  const boards = lines.reduce((acc, line) => {
+    // console.log(line);
+    if (line === '') {
+      acc.push([]);
+    } else {
+      acc[acc.length - 1].push(getNumbersFromLine(line));
+    }
+    return acc;
+  }, []);
+  // console.log(boards);
+  return {
+    drawingNumbers,
+    boards
+  }
+};
+
+export default parse;
+
