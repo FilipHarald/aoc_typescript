076a2df878624436d637dcbcf60803ac0adaff50
FilipHarald <filip.harald@gmail.com>
2022-05-25 22:17:14 +0200

    some more work on day4

--- src/solvers/day4/index.ts
+++ src/solvers/day4/index.ts
@@ -1,14 +1,30 @@
 import parse from './parser';
 
 const NUMBER = '4';
+const MARK_PREFIX = 'x';
 
 const solve = (input: string) => {
-  const {drawingNumbers, boards} = parse(input);
-  const {winnerIndex, drawnNumbers} = drawingNumbers.reduce((acc, nbr) => {
-    // TODO: if winner: just return
-    const mostMarksInRows = boards.map(board => {
-      // TODO: find and mark
-      // TODO: return amount of marks in "best" row
+  let {drawingNumbers, boards} = parse(input);
+  const {winnerIndex} = drawingNumbers.reduce((acc, drawnNumber) => {
+    if (acc.winnerIndex !== undefined) return acc;
+    let mostMarksColOrRow = 0;
+    boards = boards.map(board => {
+      const newBoard = board.map(row => {
+        const marksInColCounters = [0,0,0,0,0];
+        let mostMarksInRowCounter = 0;
+        const newRow = row.map((nbr, index) => {
+          if (drawnNumber === nbr || nbr[0] === MARK_PREFIX) {
+            marksInColCounters[index] =+ 1;
+            mostMarksInRowCounter =+ 1;
+            return MARK_PREFIX + drawnNumber;
+          } else {
+            return nbr;
+          }
+        });
+        return newRow;
+      });
+      // TODO: update mostMarksColOrRow
+      return newBoard;
     });
     // TODO: if marks === 5 set winnerIndex
     return acc;
