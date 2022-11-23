9cda23842e306103f98375a2808fa327dd1d3698
filip <filip.harald@gmail.com>
2022-11-24 00:08:46 +0100

    almost working

--- src/solvers/day13/index.ts
+++ src/solvers/day13/index.ts
@@ -2,23 +2,63 @@ import { Day } from "../../types";
 
 const NUMBER = "13";
 
-type ParsedInput = { points: [number, number][]; folds: ["x" | "y", number][] };
+type Point = { x: number; y: number };
+type ParsedInput = {
+  dots: Point[];
+  folds: { alongAxis: "x" | "y"; value: number }[];
+};
 
 function parse(input: string): ParsedInput {
   const lines = input.split("\n");
-  return lines.reduce((acc, line) => {
-
-    if (line.startsWith('f')) {
-      // TODO
-    }
-
-    return acc;
-  }, {points: [], folds: []});
+  return lines.reduce(
+    (acc, line) => {
+      if (line.startsWith("f")) {
+        const [alongAxis, value] = line.split(" ")[2].split("=");
+        acc.folds.push({
+          alongAxis,
+          value: +value,
+        });
+      } else {
+        const [x, y] = line.split(",");
+        acc.dots.push({ x: +x, y: +y });
+      }
+      return acc;
+    },
+    { dots: [], folds: [] }
+  );
 }
 
 const solve = (input: string) => {
-  const { points, folds } = parse(input);
-  return 0;
+  const { dots, folds } = parse(input);
+  const firstFold = folds[0];
+  const finalDots = dots.reduce((acc, d) => {
+    let newDot: Point;
+    if (d.x === 6) {
+      console.log(firstFold);
+      console.log(d);
+    }
+    const dotAxisValue = d[firstFold.alongAxis];
+    const isOutsideFold = dotAxisValue > firstFold.value;
+    if (isOutsideFold) {
+      const distanceFromAxis = dotAxisValue - firstFold.value;
+      const newValue = dotAxisValue - distanceFromAxis;
+      if (firstFold.alongAxis === "x") {
+        newDot = { x: newValue, y: d.y };
+      } else {
+        newDot = { x: d.x, y: newValue };
+      }
+    } else {
+      newDot = d;
+    }
+    if (!newDot.y) {
+      console.log(newDot);
+    }
+    if (!acc.find((dot: Point) => dot.x === newDot.x && dot.y === newDot.y)) {
+      acc.push(newDot);
+    }
+    return acc;
+  }, []);
+  return finalDots.length;
 };
 
 export default {
